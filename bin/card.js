e855858dad31a87f8a2a3f18aa8dd538b13fa41a
Conlin Durbin <conlin.durbin@whatnot.com>
2025-05-01 12:59:42 -0400

    Update card

--- bin/card.js
+++ bin/card.js
@@ -2,97 +2,108 @@
 // 👆 Used to tell Node.js that this is a CLI tool
 
 // Pull in our modules
-import { Chalk, supportsColor } from 'chalk';
-import boxen from 'boxen';
+import { Chalk, supportsColor } from "chalk";
+import boxen from "boxen";
 
 // Load our data
 const {
-  name,
-  handle,
-  tagline,
-  jobTitle,
-  company,
-  twitter,
-  github,
-  linkedin,
-  web,
-  card
+	name,
+	handle,
+	tagline,
+	jobTitle,
+	company,
+	twitter,
+	github,
+	linkedin,
+	web,
+	card,
+	email,
 } = {
-  handle: "wuz",
-  name: "Conlin Durbin",
-  tagline: "✨ has-been wizard ✨",
-  jobTitle: "Tech Lead Manager",
-  company: "Agora/Payscale",
-  web: "wuz.sh",
-  twitter: "wuz_tweets",
-  insta: "wuz_snaps",
-  github: "wuz",
-  linkedin: "wuz",
-  card: "wuz"
+	handle: "wuz",
+	name: "Conlin Durbin",
+	tagline: "✨ Typescripter. TTRPG-er. ✨",
+	jobTitle: "Senior Frontend Engineer",
+	company: "Whatnot",
+	web: "wuz.sh",
+	twitter: "itswuz",
+	bsky: "wuz.sh",
+	github: "wuz",
+	linkedin: "wuz",
+	card: "wuz",
+	email: "c@wuz.sh",
 };
 
-const chalk = new Chalk({level: 2});
-
+const chalk = new Chalk({ level: 2 });
 
 // Text + chalk definitions
 const boxData = {
-  handle: chalk.magenta.bold(handle),
-  name: chalk.magenta(name),
-  work: chalk.white.bold(jobTitle) + " at " + chalk.green.bold(company),
-  tagline: chalk.magenta(tagline),
-  twitter: chalk.cyan.bold(twitter),
-  github: chalk.red.bold(github),
-  linkedin: chalk.blue.bold(linkedin),
-  web: chalk.yellow.bold(web),
-  npx: chalk.white.bold(card),
-  urlTwitter: chalk.dim('https://twitter.com/'),
-  urlGithub: chalk.dim('https://github.com/'),
-  urlLinkedIn: chalk.dim('https://linkedin.com/in/'),
-  urlWeb: chalk.dim('https://'),
-  labelWork: chalk.white('Work:'.padStart(9, ' ')),
-  labelTwitter: chalk.white('Twitter:'.padStart(9, ' ')),
-  labelGitHub: chalk.white('GitHub:'.padStart(9, ' ')),
-  labelLinkedIn: chalk.white('LinkedIn:'),
-  labelWeb: chalk.white('Web:'.padStart(9, ' ')),
-  labelCard: chalk.white('Card:'.padStart(9, ' '))
-}
+	handle: chalk.magenta.bold(handle),
+	name: chalk.magenta(name),
+	email: chalk.yellow(email),
+	work: chalk.white.bold(jobTitle) + " at " + chalk.yellow.bold(company),
+	tagline: chalk.magenta(tagline),
+	twitter: chalk.cyan.bold(twitter),
+	github: chalk.red.bold(github),
+	bsky: chalk.blue.bold(github),
+	linkedin: chalk.green.bold(linkedin),
+	web: chalk.yellow.bold(web),
+	npx: chalk.white.bold(card),
+	urlTwitter: chalk.dim("https://twitter.com/"),
+	urlBsky: chalk.dim("https://bsky.app/profile/"),
+	urlGithub: chalk.dim("https://github.com/"),
+	urlLinkedIn: chalk.dim("https://linkedin.com/in/"),
+	urlWeb: chalk.dim("https://"),
+	labelWork: chalk.blue(" work".padEnd(9, " ")),
+	labelTwitter: chalk.blue(" twitter".padEnd(9, " ")),
+	labelBsky: chalk.blue(" bluesky".padEnd(9, " ")),
+	labelGitHub: chalk.blue(" github".padEnd(9, " ")),
+	labelLinkedIn: chalk.blue(" linkedin".padEnd(9, " ")),
+	labelWeb: chalk.blue(" web".padEnd(9, " ")),
+	labelEmail: chalk.blue(" email".padEnd(9, " ")),
+	separator: chalk.dim("::"),
+};
 
 // Actual strings we're going to output
-const working = `${boxData.labelWork}  ${boxData.work}`
-const twittering = `${boxData.labelTwitter}  ${boxData.urlTwitter}${boxData.twitter}`
-const githubing = `${boxData.labelGitHub}  ${boxData.urlGithub}${boxData.github}`
-const linkedining = `${boxData.labelLinkedIn}  ${boxData.urlLinkedIn}${boxData.linkedin}`
-const webing = `${boxData.labelWeb}  ${boxData.urlWeb}${boxData.web}`
-const carding = `${boxData.labelCard}  ${chalk.red('npx')} ${boxData.npx}`
+const working = `${boxData.labelWork} ${boxData.separator} ${boxData.work}`;
+const bskying = `${boxData.labelBsky} ${boxData.separator} ${boxData.urlBsky}${boxData.bsky}`;
+const githubing = `${boxData.labelGitHub} ${boxData.separator} ${boxData.urlGithub}${boxData.github}`;
+const linkedining = `${boxData.labelLinkedIn} ${boxData.separator} ${boxData.urlLinkedIn}${boxData.linkedin}`;
+const webing = `${boxData.labelWeb} ${boxData.separator} ${boxData.urlWeb}${boxData.web}`;
+const emailing = `${boxData.labelEmail} ${boxData.separator} ${boxData.email}`;
 
+const title = `${boxData.name} (${boxData.handle})`;
 
-const title = `${boxData.name} ${chalk.gray.dim("\\\\")} ${boxData.handle}`;
+const footer = chalk.dim(
+	`      run ${chalk.yellow(`npx wuz`)} anytime to see this card`,
+);
 
 // Put all our output together into a single variable so we can use boxen effectively
 const output = `
-              ${boxData.tagline} 
+          ${boxData.tagline} 
   
 ${working}
 ${webing}
-${twittering}
+${bskying}
 ${githubing}
 ${linkedining}
-${carding}
+${emailing}
+
+${footer}
 `;
 
 // Define options for Boxen
 const options = {
-  padding: {
-    top: 0,
-    bottom: 1,
-    left: 1, 
-    right: 2,
-  },
-  margin: 1,
-  borderStyle: 'round',
-  borderColor: 'magenta',
-  title,
-  titleAlignment: 'center',
-}
+	padding: {
+		top: 0,
+		bottom: 0,
+		left: 1,
+		right: 2,
+	},
+	margin: 1,
+	borderStyle: "round",
+	borderColor: "yellow",
+	title,
+	titleAlignment: "center",
+};
 
 console.log(boxen(output, options));
